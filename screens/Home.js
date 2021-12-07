717c9de38a4958400fcb75cfb64cd1e33d0b16eb
QTrongIT <trandinhquangtrong@gmail.com>
2021-12-07 15:34:12 +0700

    update1

--- screens/Home.js
+++ screens/Home.js
@@ -2,19 +2,49 @@ import React from 'react';
 import {
     View,
     Text,
-    TouchableOpacity
+    FlatList,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Image,
+    ImageBackground,
+    Animated,
+    ScrollView,
+    SafeAreaView,
 } from 'react-native';
 
+import {dummyData,COLORS, SIZES,FONTS,icons,images}from '../constants';
+  
+
+
 const Home = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("MovieDetail")}
-            >
-                <Text>Navigate to MovieDetail</Text>
-            </TouchableOpacity>
+function renderHeader(){
+    return(
+        <View
+        style={{
+            flexDirection:'row',
+            justifyContent:'space-between',
+            alignItems:'center',
+            paddingHorizontal: SIZES.padding
+        }}
+        >
+            {/* {profile} */}
+            
+            {/* {Screen Mirror} */}
+
         </View>
+        )
+}
+
+    return (
+     <SafeAreaView
+     style={{
+         flex: 1,
+         backgroundColor: COLORS.black
+     }}
+     >
+         {renderHeader()}
+
+     </SafeAreaView>
     )
 }
 
